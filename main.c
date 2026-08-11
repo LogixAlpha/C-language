18c9b962acb6580a779a366623e19e48c18aa2f9
alpha <ayoub.ouaddi.yt@gmail.com>
2026-08-11 23:17:50 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
 #include <stdio.h>
 
 int main()
@@ -14,7 +6,7 @@ int main()
     float cost = 10;
     float total = items * cost ;
     char currency[] = "DH";
-    
+    // print Items / cost / total
     printf("Items: %i \n",items);
     printf("Cost of one item: %.2f %s \n",cost,currency);
     printf("Total Cost: %.2f %s",total,currency);
