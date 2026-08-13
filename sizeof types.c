7af2b5acb723837e6963d7c09854917cd844a8b0
alpha <ayoub.ouaddi.yt@gmail.com>
2026-08-13 23:28:09 +0200

    Update sizeof types.c

--- sizeof types.c
+++ sizeof types.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 int main() {
-  int num;
-  float fnum;
-  double dnum;
-  char letter;
-  //char name[];
-  printf("%zu" , sizeof(num));
-  printf("%zu" , sizeof(fnum));
-  printf("%zu" , sizeof(dnum));
-  printf("%zu" , sizeof(letter));
-  //printf("%zu" , sizeof(name));
+  int num = 1;
+  float fnum = 1.00;
+  double dnum = 1.0001;
+  char letter = 'A';
+  char name[] = "Example";
+  printf("%zu [%i] \n" , sizeof(num),num);
+  printf("%zu [%f] \n" , sizeof(fnum),fnum);
+  printf("{Float with Precize 2 digits after comma} : %zu [%.2f] \n",sizeof(fnum),fnum);
+  printf("%zu [%lf] \n" , sizeof(dnum),dnum);
+  printf("%zu [%c] \n" , sizeof(letter),letter);
+  printf("%zu [%s]" , sizeof(name),name);
   return 0;
 
 
